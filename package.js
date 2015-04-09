191f25fb245f2163deaa9fb402879fb288104eeb
Ryan Yeske <rcyeske@gmail.com>
2015-04-09 00:50:04 -0700

    refer to local readme in documentation

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   git: 'git@github.com:rcy/meteor-tacit.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: "https://github.com/rcy/meteor-tacit/blob/master/README.md"
+  documentation: "README.md"
 });
 
 Package.onUse(function(api) {
