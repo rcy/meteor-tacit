5d2bbb34466f37e1b7a24e9695a80cc8ea6edbf5
Ryan Yeske <rcyeske@gmail.com>
2015-04-09 00:54:57 -0700

    use https git url

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'rcy:tacit',
-  version: '1.0.0',
+  version: '1.0.0_2',
   // Brief, one-line summary of the package.
   summary: 'CSS Framework for Dummies',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:rcy/meteor-tacit.git',
+  git: 'https://github.com/rcy/meteor-tacit.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: "README.md"
